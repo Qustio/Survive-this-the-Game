e74fdff336fd043f8028c86a2562369818fd5465
Qustio <qustioof@gmail.com>
2022-09-29 14:26:42 +0300

    Draw fps in debug mod only

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -45,9 +45,12 @@ impl Game {
     }
 
     pub async fn render(&mut self) {
-        clear_background(RED);
         self.fps = get_fps();
-        draw_text(&format!("{}", self.fps), 100.0, 100.0, 36.0, WHITE);
+        clear_background(RED);
+        // Draw fps in debug
+        if cfg!(debug_assertions) {
+            draw_text(&format!("{}", self.fps), 10.0, 20.0, 36.0, WHITE);
+        }
         draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
         draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
